27b219bf6d282e2951059805f12ed86889675092
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-08-21 10:31:31 -0300

    feat: exported chronometer API

--- src/index.js
+++ src/index.js
@@ -2,43 +2,52 @@ let horas = 0;
 let minutos = 0;
 let segundos = 0;
 
-function zerar(marca){
+function zerar(marca) {
     marca = 0;
 }
 
-function medidasMarcas(){
+function medidasMarcas() {
     segundos++;
-    if(segundos === 60){
+    if (segundos === 60) {
         zerar(segundos);
         minutos++;
 
-        if(minutos === 60){
+        if (minutos === 60) {
             zerar(minutos);
             horas++;
         }
-        
-        if(horas === 23 &&  minutos === 59 && segundos === 59 ){
+
+        if (horas === 23 && minutos === 59 && segundos === 59) {
             reiniciar();
         }
     }
-    getElementbyId("Horas").innerHTML = horas + " : ";
-    getElementbyId("Minutos").innerHTML = minutos + " : "; 
-    getElementbyId("Segundos").innerHTML = segundos;   
-}
-function iniciar(){
-    intervalo = setInterval(tempoMedidas, 1000);
+    getElementbyId("hours").innerHTML = horas + " : ";
+    getElementbyId("minutes").innerHTML = minutos + " : ";
+    getElementbyId("seconds").innerHTML = segundos;
 }
 
-
-function zerarTodos(marca1, marca2, marca3){
+function zerarTodos(marca1, marca2, marca3) {
     marca1 = marca2 = marca3 = 0;
 }
 
-function reiniciar(){
+// API -------------------------------------------------------------------------
+
+function reiniciar() {
     clearInterval(intervalo);
     zerarTodos(horas, minutos, segundos);
     iniciar();
 }
-function parar(){
+
+function iniciar() {
+    intervalo = setInterval(medidasMarcas, 1000);
+}
+
+function parar() {
     clearInterval(intervalo);
 }
+
+export const chronometer = {
+    reiniciar,
+    iniciar,
+    parar
+};
