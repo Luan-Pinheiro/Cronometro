27c6e8af04327b1ce6084ecf4211bfc866bb82df
Luan-Pinheiro <luan.p12@hotmail.com>
2022-08-21 10:19:04 -0300

    add logic to cronometer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-const teste = () => {
-    console.log('teste2');
-};
+let horas = 0;
+let minutos = 0;
+let segundos = 0;
 
-export { teste };
+function zerar(marca){
+    marca = 0;
+}
+
+function medidasMarcas(){
+    segundos++;
+    if(segundos === 60){
+        zerar(segundos);
+        minutos++;
+
+        if(minutos === 60){
+            zerar(minutos);
+            horas++;
+        }
+        
+        if(horas === 23 &&  minutos === 59 && segundos === 59 ){
+            reiniciar();
+        }
+    }
+    getElementbyId("Horas").innerHTML = horas + " : ";
+    getElementbyId("Minutos").innerHTML = minutos + " : "; 
+    getElementbyId("Segundos").innerHTML = segundos;   
+}
+function iniciar(){
+    intervalo = setInterval(tempoMedidas, 1000);
+}
+
+
+function zerarTodos(marca1, marca2, marca3){
+    marca1 = marca2 = marca3 = 0;
+}
+
+function reiniciar(){
+    clearInterval(intervalo);
+    zerarTodos(horas, minutos, segundos);
+    iniciar();
+}
+function parar(){
+    clearInterval(intervalo);
+}
