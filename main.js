9479c716538f83c7c6fad0cf131086b1486bd12a
Gustavo Maltez <gustavomaltez37@hotmail.com>
2022-08-21 10:32:05 -0300

    feat: added chronometer buttons link

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 import './src/styles';
 
-import { teste } from './src/index';
-teste();
+import { chronometer } from './src/index';
+
+const pauseButton = document.getElementById('pause');
+const startButton = document.getElementById('start');
+const resetButton = document.getElementById('reset');
+
+pauseButton.addEventListener('click', chronometer.parar);
+startButton.addEventListener('click', chronometer.iniciar);
+resetButton.addEventListener('click', chronometer.reiniciar);
